ebaeb7d928ac1b98533543d25752308403e224bf
shaoyuan <84977043@qq.com>
2020-11-05 17:01:10 +0800

    add samples

---
+++ sample/template/client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net"
+
+	"github.com/shaoyuan1943/gouxp"
+)
+
+type Client struct {
+	*gouxp.ClientConn
+}
+
+func (client *Client) OnClosed(err error)          {}
+func (client *Client) OnNewDataComing(data []byte) {}
+func (client *Client) OnReady()                    {}
+
+func main() {
+	addr := "127.0.0.1:9000"
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", nil)
+	if err != nil {
+		return
+	}
+
+	client := &Client{}
+	client.ClientConn = gouxp.NewClientConn(conn, udpAddr, client)
+	// client.SetMTU
+	// client.SetUpdateInterval
+	// client.SetWindow
+	// client.UseCryptoCodec
+	// client.EnableFEC
+	client.Start()
+
+	// client.Close
+	return
+}
---
+++ sample/template/server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net"
+
+	"github.com/shaoyuan1943/gouxp"
+)
+
+type Session struct {
+	*gouxp.ServerConn
+}
+
+func (u *Session) OnClosed(err error)          {}
+func (u *Session) OnNewDataComing(data []byte) {}
+func (u *Session) OnReady()                    {}
+
+type Server struct {
+	*gouxp.Server
+}
+
+func (s *Server) OnNewConnComing(conn *gouxp.ServerConn) {
+	session := &Session{}
+	session.ServerConn = conn
+	session.SetConnHandler(session)
+	// session.SetMTU
+	// session.SetUpdateInterval
+	// session.SetWindow
+	// session.UseCryptoCodec
+	// session.EnableFEC
+}
+
+func (s *Server) OnConnClosed(conn *gouxp.ServerConn, err error) {}
+func (s *Server) OnClosed(err error)                             {}
+
+func main() {
+	addr := "127.0.0.1:9000"
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return
+	}
+
+	server := &Server{}
+	server.Server = gouxp.NewServer(conn, server, 2)
+
+	// server.UseCryptoCodec
+	server.Start()
+
+	// server.Close
+	return
+}
